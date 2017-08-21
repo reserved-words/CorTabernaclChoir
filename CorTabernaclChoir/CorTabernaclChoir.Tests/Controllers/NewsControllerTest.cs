b642c447f91c735480fd699e9b591b42b665490c
Rhian <rhian.gwawr@gmail.com>
2017-08-21 18:20:35 +0100

    Task #97: Added unit tests for NewsController Add methods

--- CorTabernaclChoir/CorTabernaclChoir.Tests/Controllers/NewsControllerTest.cs
+++ CorTabernaclChoir/CorTabernaclChoir.Tests/Controllers/NewsControllerTest.cs
@@ -8,12 +8,15 @@ using System.Collections.Generic;
 using System.Web.Mvc;
 using CorTabernaclChoir.Common;
 using CorTabernaclChoir.Interfaces;
+using FluentAssertions;
 
 namespace CorTabernaclChoir.Tests.Controllers
 {
     [TestClass]
     public class NewsServiceTest
     {
+        private const string TestErrorMessage = "Some error message";
+        private const string TestPropertyName = "PropName";
         private const int TestPageNo = 4;
         private const int TestId = 23;
 
@@ -22,19 +25,27 @@ namespace CorTabernaclChoir.Tests.Controllers
 
         private Mock<IPostsService> _mockService;
         private Mock<ICultureService> _mockCultureService;
+        private Mock<IMessageContainer> _mockMessageContainer;
 
-        private NewsController GetSubjectUnderTest()
+        private NewsController GetSubjectUnderTest(bool isModelValid = true)
         {
             _mockService = new Mock<IPostsService>();
             _mockCultureService = new Mock<ICultureService>();
+            _mockMessageContainer = new Mock<IMessageContainer>();
 
             var mockLogger = new Mock<ILogger>();
-            var mockMessageContainer = new Mock<IMessageContainer>();
-
+            
             _mockService.Setup(h => h.Get(TestPageNo, PostType.News)).Returns(_mockPostsViewModel);
             _mockService.Setup(h => h.Get(TestId)).Returns(_mockPostViewModel);
 
-            return new NewsController(_mockService.Object, _mockCultureService.Object, mockLogger.Object, mockMessageContainer.Object);
+            var subjectUnderTest = new NewsController(_mockService.Object, _mockCultureService.Object, mockLogger.Object, _mockMessageContainer.Object);
+
+            if (!isModelValid)
+            {
+                subjectUnderTest.ModelState.AddModelError(TestPropertyName, TestErrorMessage);
+            }
+
+            return subjectUnderTest;
         }
 
         [TestMethod]
@@ -71,6 +82,58 @@ namespace CorTabernaclChoir.Tests.Controllers
             Assert.IsNotNull(result);
             Assert.AreEqual(_mockPostViewModel, result.Model);
             Assert.AreEqual("", result.ViewName);
+        }
+
+        [TestMethod]
+        public void Add_ReturnsCorrectView()
+        {
+            // Arrange
+            var sut = GetSubjectUnderTest();
+
+            // Act
+            var result = sut.Add() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+
+            var model = result.Model as Post;
+            Assert.IsNotNull(model);
+            Assert.AreEqual(0, model.Id);
+            Assert.AreEqual(model.Type, PostType.News);
+            Assert.AreEqual("", result.ViewName);
+        }
+
+        [TestMethod]
+        public void AddModel_GivenValidModel_CallsServiceAndRedirects()
+        {
+            // Arrange
+            var model = new Post();
+            var subjectUnderTest = GetSubjectUnderTest();
+
+            // Act
+            var result = subjectUnderTest.Add(model) as RedirectToRouteResult;
+
+            // Assert
+            _mockService.Verify(s => s.Save(model), Times.Once);
+            _mockMessageContainer.Verify(m => m.AddSaveSuccessMessage());
+            result.Should().NotBeNull();
+        }
+
+        [TestMethod]
+        public void AddModel_GivenInvalidModel_ReturnsError()
+        {
+            // Arrange
+            var model = new Post();
+            var subjectUnderTest = GetSubjectUnderTest(false);
+
+            // Act
+            var result = subjectUnderTest.Add(model) as ViewResult;
+
+            // Assert
+            _mockService.Verify(s => s.Save(It.IsAny<Post>()), Times.Never);
+            _mockMessageContainer.Verify(m => m.AddSaveErrorMessage());
+            result.Model.Should().Be(model);
+            result.ViewName.Should().Be("");
         }
     }
 }
